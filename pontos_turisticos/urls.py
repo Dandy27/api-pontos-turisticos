94f8152e9d78a1d0aa34e831c3ad03576d3383e3
Dandy27 <danielsbarbosa27@gmail.com>
2020-10-04 20:39:08 -0300

    Add enderecos/viewset e serializers

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -21,6 +21,7 @@ from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracoesViewSet
 from avaliacoes.api.viewsets import AvaliacoesViewSet
 from comentarios.api.viewsets import ComentariosViewSet
+from enderecos.api.viewsets import EnderecosViewSet
 
 
 
@@ -29,6 +30,7 @@ router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracoesViewSet)
 router.register(r'avaliacoes', AvaliacoesViewSet)
 router.register(r'comentarios', ComentariosViewSet)
+router.register(r'enderecos', EnderecosViewSet)
 
 
 urlpatterns = [
